98e1d1bd5c851e06ee52ec8569441e760c67e880
LeVietDuc <1612118@student.hcmus.edu.vn>
2018-12-11 23:10:29 +0700

    Source code các bảng
    Bảng phân bố trọng tài, nhập kết quả trận đấu, nhập thông số trận đấu, vua phá lưới

---
+++ 04_Implement/Source_Code/Football_League_Manager/Football_League_Manager/NhapThongSoTranDau.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Football_League_Manager
+{
+    /// <summary>
+    /// Interaction logic for NhapThongSoTranDau.xaml
+    /// </summary>
+    public partial class NhapThongSoTranDau : Window
+    {
+        public NhapThongSoTranDau()
+        {
+            InitializeComponent();
+        }
+
+        public string TenDoiNha { get; set; }
+        public string TenDoiKhach { get; set;}
+        public string MaTran { get; set; }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var iteams = new List<string> { TenDoiNha,TenDoiKhach };
+            DBcomboBox.ItemsSource = iteams;
+            var thevang = new List<int> { 1, 2 };
+            comboBox2.ItemsSource = thevang;
+            var sobanthang = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            comboBox3.ItemsSource = sobanthang;
+            //DBcomboBox.SelectedIndex = 0;
+        }
+        List<CauThu> DSCauThu = new List<CauThu>();
+        private void DBcomboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataProvider dataProvider = new DataProvider();
+            string ten = DBcomboBox.SelectedItem.ToString();
+            SqlCommand sqlCommand = dataProvider.ExcuteQuery("select CT.TenCT, CT.SoAo,CT.MaCT,CT.MaDB,CT.SoTheVang from DoiBong DB, CauThu CT where DB.MaDB = CT.MaDB and DB.TenDB = @TenDoiBong", new object[] { ten });
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+       
+            while (sqlDataReader.Read())
+            {
+                string TenCauThu = sqlDataReader.GetString(0);
+                int SoAo = sqlDataReader.GetInt32(1);
+                string MaCT = sqlDataReader.GetString(2);
+                string MaDB = sqlDataReader.GetString(3);
+                var SoTheVang= sqlDataReader.GetInt32(4);
+                
+                CauThu CT=new CauThu();
+                CT.TenCauThu = TenCauThu;
+                CT.SoAo = SoAo;
+                CT.MaCauThu = MaCT;
+                CT.MaDB = MaDB;
+                CT.SoTheVang = SoTheVang;
+                DSCauThu.Add(CT);
+            }
+            comboBox1.ItemsSource = DSCauThu;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            int SoTheVang = DSCauThu[comboBox1.SelectedIndex].SoTheVang + int.Parse(comboBox2.SelectedItem.ToString());
+            string MaCT = DSCauThu[DBcomboBox.SelectedIndex].MaCauThu;
+            string MaDB = DSCauThu[DBcomboBox.SelectedIndex].MaDB;
+            int SoBanThang =int.Parse(comboBox3.SelectedItem.ToString());
+            string maTran = MaTran;
+            DataProvider dataProvider = new DataProvider();
+            int a = dataProvider.ExcuteNonQuery("insert into GhiBan values ( @MaCT , @MaTran , @MaDB , @soban )",new object[] {MaCT,maTran,MaDB,SoBanThang});
+            
+            int b = dataProvider.ExcuteNonQuery("update CauThu set SoTheVang= @SoTheVang where MaDB= @MaDB and MaCT= @MaCT ", new object[] { SoTheVang, MaDB, MaCT });
+            MessageBox.Show("Lưu thành công");
+
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
+
---
+++ 04_Implement/Source_Code/Football_League_Manager/Football_League_Manager/PhanBoTrongTai.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Football_League_Manager
+{
+    /// <summary>
+    /// Interaction logic for PhanBoTrongTai.xaml
+    /// </summary>
+    public partial class PhanBoTrongTai : Window
+    {
+        public PhanBoTrongTai()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var iteams = new List<string> { "Vòng 1", "Vòng 2", "Vòng 3", "Vòng 4", "Vòng 5", "Vòng 6", "Vòng 7", "Vòng 8", "Vòng 9" };
+            VDComboBox.ItemsSource = iteams;
+        }
+        class DataObject
+        {
+            public string TenDoiNha { get; set; }
+            public string TenDoiKhach { get; set; }
+            public string MaTran { get; set; }
+        }
+
+
+        private void VDComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            List<DataObject> DSTranDau;
+            DataProvider dataProvider = new DataProvider();
+            SqlCommand sqlCommand = dataProvider.ExcuteQuery("select DB.TenDB,DB1.TenDB,LTD.MaTran from DoiBong DB, LichThiDau LTD, DoiBong DB1 where DB.MaDB = LTD.MaDoi1 and DB1.MaDB = LTD.MaDoi2 and LTD.VongDau = @Vong ", new object[] { VDComboBox.SelectedIndex + 1 });
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+            DSTranDau = new List<DataObject>();
+
+            while (sqlDataReader.Read())
+            {
+                string TenDoiNha = sqlDataReader.GetString(0);
+                string TenDoiKhach = sqlDataReader.GetString(1);
+                string MaTran = sqlDataReader.GetString(2);
+                DataObject data = new DataObject();
+                data.TenDoiNha = TenDoiNha;
+                data.TenDoiKhach = TenDoiKhach;
+                data.MaTran = MaTran;
+                DSTranDau.Add(data);
+            }
+            TDComboBox.ItemsSource = DSTranDau;
+        }
+        DanhSachTrongTai TT;
+        private void TDComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            TT = new DanhSachTrongTai();
+            TT.LoadDSTrongTai();
+
+            TT1comboBox.ItemsSource = TT.DanhSachTrongTais.Values;
+
+        }
+
+
+        private void TT1comboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            TT = new DanhSachTrongTai();
+            TT.LoadDSTrongTai();
+            string matt = TT.DanhSachTrongTais.ElementAt(TT1comboBox.SelectedIndex).Key;
+            TT.DanhSachTrongTais.Remove(matt);
+            TT2comboBox.ItemsSource = TT.DanhSachTrongTais.Values;
+        }
+
+        private void TT2comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TT = new DanhSachTrongTai();
+            TT.LoadDSTrongTai();
+            string matt = TT.DanhSachTrongTais.ElementAt(TT1comboBox.SelectedIndex).Key;
+            TT.DanhSachTrongTais.Remove(matt);
+            string matt1 = TT.DanhSachTrongTais.ElementAt(TT2comboBox.SelectedIndex).Key;
+            TT.DanhSachTrongTais.Remove(matt1);
+            TT3comboBox.ItemsSource = TT.DanhSachTrongTais.Values;
+        }
+
+        private void TT3comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            TT = new DanhSachTrongTai();
+            TT.LoadDSTrongTai();
+            string matt = TT.DanhSachTrongTais.ElementAt(TT1comboBox.SelectedIndex).Key;
+            TT.DanhSachTrongTais.Remove(matt);
+            string matt1 = TT.DanhSachTrongTais.ElementAt(TT2comboBox.SelectedIndex).Key;
+            TT.DanhSachTrongTais.Remove(matt1);
+            string matt2 = TT.DanhSachTrongTais.ElementAt(TT3comboBox.SelectedIndex).Key;
+            TT.DanhSachTrongTais.Remove(matt2);
+
+            TT4comboBox.ItemsSource = TT.DanhSachTrongTais.Values;
+        }
+    }
+}
+
+
