caf94106d7c8dc710200d1d6be017f6497c44396
Unknown <1612431@student.hcmus.edu.vn>
2018-12-21 23:11:50 +0700

    Update source code

--- 04_Implement/Source_Code/Football_League_Manager/Football_League_Manager/TraCuuBangXepHang.xaml.cs
+++ 04_Implement/Source_Code/Football_League_Manager/Football_League_Manager/TraCuuBangXepHang.xaml.cs
@@ -80,112 +80,120 @@ namespace Football_League_Manager
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             DataProvider dataProvider = new DataProvider();
-            SqlCommand sqlCommand = dataProvider.ExcuteQuery("select ltd.MaTran, db1.TenDB, db2.TenDB, kq.SoBanDoi1, kq.SoBanDoi2 from LichThiDau ltd, KetQua kq, DoiBong db1, DoiBong db2 " +
-            "where(ltd.MaDoi1 = db1.MaDB  and  ltd.MaDoi2 = db2.MaDB) and kq.MaTran = ltd.MaTran");
+            SqlCommand sqlCommand = dataProvider.ExcuteQuery("select TenDB from DoiBong");
             SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
 
-            TranDaus = new List<TranDau>();
-            while (sqlDataReader.Read())
-            {
-                TranDau tranDau = new TranDau();
-                tranDau.MaTranDau = sqlDataReader.GetString(0);
-                tranDau.TenDoiA = sqlDataReader.GetString(1);
-                tranDau.TenDoiB = sqlDataReader.GetString(2);
-                tranDau.TiSoDoiA = sqlDataReader.GetInt32(3);
-                tranDau.TiSoDoiB = sqlDataReader.GetInt32(4);
-                TranDaus.Add(tranDau);
-            }
-
-            sqlCommand = dataProvider.ExcuteQuery("select TenDB from DoiBong");
-            sqlDataReader = sqlCommand.ExecuteReader();
-
             List<string> DoiBongs = new List<string>();
+            int soDoi = 0;
             while (sqlDataReader.Read())
             {
+                soDoi++;
                 DoiBongs.Add(sqlDataReader.GetString(0));
             }
 
-            var list = new List<DataObject>();
-            for (int i = 0; i < DoiBongs.Count; i++)
+            if (soDoi == 0) MessageBox.Show("Hãy thêm đội bóng trước khi tra cứu bảng xếp hạng nhé!");
+            else
             {
-                DataObject dataObject = new DataObject();
+                sqlCommand = dataProvider.ExcuteQuery("select ltd.MaTran, db1.TenDB, db2.TenDB, kq.SoBanDoi1, kq.SoBanDoi2 from LichThiDau ltd, KetQua kq, DoiBong db1, DoiBong db2 " +
+              "where(ltd.MaDoi1 = db1.MaDB  and  ltd.MaDoi2 = db2.MaDB) and kq.MaTran = ltd.MaTran");
+                sqlDataReader = sqlCommand.ExecuteReader();
 
-                dataObject.TenDoiBong = DoiBongs[i];
-
-                dataObject.SoTran = 0;
-                foreach(var tranDau in TranDaus)
+                TranDaus = new List<TranDau>();
+                int soTran = 0;
+                while (sqlDataReader.Read())
                 {
-                    if (DoiBongs[i] == tranDau.TenDoiA || DoiBongs[i] == tranDau.TenDoiB)
+                    soTran++;
+                    TranDau tranDau = new TranDau();
+                    tranDau.MaTranDau = sqlDataReader.GetString(0);
+                    tranDau.TenDoiA = sqlDataReader.GetString(1);
+                    tranDau.TenDoiB = sqlDataReader.GetString(2);
+                    tranDau.TiSoDoiA = sqlDataReader.GetInt32(3);
+                    tranDau.TiSoDoiB = sqlDataReader.GetInt32(4);
+                    TranDaus.Add(tranDau);
+                }
+
+                var list = new List<DataObject>();
+                for (int i = 0; i < DoiBongs.Count; i++)
+                {
+                    DataObject dataObject = new DataObject();
+
+                    dataObject.TenDoiBong = DoiBongs[i];
+
+                    dataObject.SoTran = 0;
+                    foreach (var tranDau in TranDaus)
                     {
-                        dataObject.SoTran += 1;
+                        if (DoiBongs[i] == tranDau.TenDoiA || DoiBongs[i] == tranDau.TenDoiB)
+                        {
+                            dataObject.SoTran += 1;
+                        }
+                    }
+
+                    if (dataObject.SoTran != 0)
+                    {
+                        dataObject.SoTranThang = 0;
+                        foreach (var tranDau in TranDaus)
+                        {
+                            if ((DoiBongs[i] == tranDau.TenDoiA && tranDau.TiSoDoiA > tranDau.TiSoDoiB)
+                                || (DoiBongs[i] == tranDau.TenDoiB && tranDau.TiSoDoiB > tranDau.TiSoDoiA))
+                            {
+                                dataObject.SoTranThang += 1;
+                            }
+                        }
+
+                        dataObject.SoTranHoa = 0;
+                        foreach (var tranDau in TranDaus)
+                        {
+                            if ((DoiBongs[i] == tranDau.TenDoiA || DoiBongs[i] == tranDau.TenDoiB) && tranDau.TiSoDoiB == tranDau.TiSoDoiA)
+                            {
+                                dataObject.SoTranHoa += 1;
+                            }
+                        }
+
+                        dataObject.SoTranThua = dataObject.SoTran - dataObject.SoTranThang - dataObject.SoTranHoa;
+
+                        dataObject.SoBanThang = 0;
+                        dataObject.SoBanThua = 0;
+                        foreach (var tranDau in TranDaus)
+                        {
+                            if (DoiBongs[i] == tranDau.TenDoiA)
+                            {
+                                dataObject.SoBanThang += tranDau.TiSoDoiA;
+                                dataObject.SoBanThua += tranDau.TiSoDoiB;
+                            }
+
+                            if (DoiBongs[i] == tranDau.TenDoiB)
+                            {
+                                dataObject.SoBanThang += tranDau.TiSoDoiB;
+                                dataObject.SoBanThua += tranDau.TiSoDoiA;
+                            }
+                        }
+
+                        dataObject.HieuSo = dataObject.SoBanThang - dataObject.SoBanThua;
+
+                        dataObject.Diem = dataObject.SoTranThang * 3 + dataObject.SoTranHoa * 1;
+
+                        list.Add(dataObject);
                     }
                 }
 
-                if (dataObject.SoTran != 0)
+
+                for (int i = 0; i < list.Count; i++)
                 {
-                    dataObject.SoTranThang = 0;
-                    foreach (var tranDau in TranDaus)
+                    for (int j = i; j < list.Count; j++)
                     {
-                        if ((DoiBongs[i] == tranDau.TenDoiA && tranDau.TiSoDoiA > tranDau.TiSoDoiB)
-                            || (DoiBongs[i] == tranDau.TenDoiB && tranDau.TiSoDoiB > tranDau.TiSoDoiA))
+                        if (list[j].SoSanh(list[i]) == 1)
                         {
-                            dataObject.SoTranThang += 1;
+                            DataObject temp = list[i];
+                            list[i] = list[j];
+                            list[j] = temp;
                         }
                     }
-
-                    dataObject.SoTranHoa = 0;
-                    foreach (var tranDau in TranDaus)
-                    {
-                        if ((DoiBongs[i] == tranDau.TenDoiA || DoiBongs[i] == tranDau.TenDoiB) && tranDau.TiSoDoiB == tranDau.TiSoDoiA)
-                        {
-                            dataObject.SoTranHoa += 1;
-                        }
-                    }
-
-                    dataObject.SoTranThua = dataObject.SoTran - dataObject.SoTranThang - dataObject.SoTranHoa;
-
-                    dataObject.SoBanThang = 0;
-                    dataObject.SoBanThua = 0;
-                    foreach (var tranDau in TranDaus)
-                    {
-                        if (DoiBongs[i] == tranDau.TenDoiA)
-                        {
-                            dataObject.SoBanThang += tranDau.TiSoDoiA;
-                            dataObject.SoBanThua += tranDau.TiSoDoiB;
-                        }
-
-                        if (DoiBongs[i] == tranDau.TenDoiB)
-                        {
-                            dataObject.SoBanThang += tranDau.TiSoDoiB;
-                            dataObject.SoBanThua += tranDau.TiSoDoiA;
-                        }
-                    }
-
-                    dataObject.HieuSo = dataObject.SoBanThang - dataObject.SoBanThua;
-
-                    dataObject.Diem = dataObject.SoTranThang * 3 + dataObject.SoTranHoa * 1;
-
-                    list.Add(dataObject);
+                    list[i].ViTri = i + 1;
                 }
+
+                BXHListView.ItemsSource = list;
+                bangXepHangs = list;
             }
-            
-            
-            for (int i = 0; i < list.Count; i++)
-            {
-                for (int j = i; j < list.Count; j++)
-                {
-                    if (list[j].SoSanh(list[i]) == 1)
-                    {
-                        DataObject temp = list[i];
-                        list[i] = list[j];
-                        list[j] = temp;
-                    }
-                }
-                list[i].ViTri = i + 1;
-            }
-
-            BXHListView.ItemsSource = list;
-            bangXepHangs = list;
         }
     }
 }
